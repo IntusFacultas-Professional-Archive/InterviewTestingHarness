70b92b62939dc07cc1147a2babf2b503267300dc
Pedro Del Moral Lopez <pedro.del_moral_lopez.1@us.af.mil>
2021-07-21 12:59:12 -0400

    Initial commit.

---
+++ README.md
@@ -0,0 +1,4 @@
+# Testing harness
+
+This is a lightweight testing harness meant to give me some of the same functionality as Jest for use
+in interviews where I can't import external libraries. 
---
+++ TestingHarness.js
@@ -0,0 +1,33 @@
+const testCases = {
+  'Test Name': {
+      actions() {
+          return null
+      },
+      expectedValue: null
+  },
+}
+const formatTest = (testName, actualValue, expectedValue) => {
+  if (actualValue === expectedValue) {
+      return `\u2713 ${testName}`
+  }
+  return `\u2718 ${testName}
+  Expected: ${expectedValue}
+  Received: ${actualValue}`
+}
+
+(function invokeTests() {
+  const testResults = [];
+  Object.entries(testCases).forEach(([testName, test]) => {
+      try {
+          const result = test.actions();
+          if(result !== test.expectedValue) {
+              testResults.push(formatTest(testName, result, test.expectedValue));
+          }
+      } catch (e) {
+          testResults.push(formatTest(testName, e, test.expectedValue))
+      }
+  })
+  if(testResults.length) {
+      testResults.forEach((result) => console.log(result))
+  }
+})()
